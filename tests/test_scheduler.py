9562851186e30dfa75d5585df3c76375691d0451
Haegdorens, Gert <gert.haegdorens@yrcw.com>
2021-02-05 05:49:38 -0600

    Move yesterday funcion of scheduler to subclass start_date for clearity

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -5,13 +5,13 @@ from datetime import datetime, timedelta
 class TestTheProjectMethods(unittest.TestCase):
     def test_yesterday_methods(self):
         now = datetime.now() - timedelta(days=1)
-        self.assertEqual(now.year, Scheduler.yesterday().year)
-        self.assertEqual(now.month , Scheduler.yesterday().month)
-        self.assertEqual(now.day, Scheduler.yesterday().day)
-        self.assertEqual(0, Scheduler.yesterday().hour)
-        self.assertEqual(0, Scheduler.yesterday().minute)
-        self.assertEqual(0, Scheduler.yesterday().second)
-        self.assertEqual(0, Scheduler.yesterday().microsecond)
+        self.assertEqual(now.year, Scheduler.StartDate.yesterday().year)
+        self.assertEqual(now.month , Scheduler.StartDate.yesterday().month)
+        self.assertEqual(now.day, Scheduler.StartDate.yesterday().day)
+        self.assertEqual(0, Scheduler.StartDate.yesterday().hour)
+        self.assertEqual(0, Scheduler.StartDate.yesterday().minute)
+        self.assertEqual(0, Scheduler.StartDate.yesterday().second)
+        self.assertEqual(0, Scheduler.StartDate.yesterday().microsecond)
 
 
 if __name__ == '__main__':
--- zeppos_airflow/scheduler.py
+++ zeppos_airflow/scheduler.py
@@ -2,8 +2,9 @@ import pendulum
 
 
 class Scheduler:
-    @staticmethod
-    def yesterday():
-        yesterday = pendulum.now("America/Chicago")
-        return yesterday.subtract(days=1, hours=yesterday.hour, minutes=yesterday.minute, seconds=yesterday.second,
-                                  microseconds=yesterday.microsecond)
+    class StartDate:
+        @staticmethod
+        def yesterday():
+            yesterday = pendulum.now("America/Chicago")
+            return yesterday.subtract(days=1, hours=yesterday.hour, minutes=yesterday.minute, seconds=yesterday.second,
+                                      microseconds=yesterday.microsecond)
