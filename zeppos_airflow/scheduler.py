3b0fbf9373ceed674b3dfdadc18a92f4bb26596e
Haegdorens, Gert <gert.haegdorens@yrcw.com>
2021-02-05 06:59:45 -0600

    Use utc_now as variable for Scheduler.StartDate.Yesterday

--- zeppos_airflow/scheduler.py
+++ zeppos_airflow/scheduler.py
@@ -1,19 +1,20 @@
 import pendulum
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 class Scheduler:
     class StartDate:
         @staticmethod
         def yesterday():
-            yesterday = pendulum.now("utc")
-            return yesterday.subtract(days=1, hours=yesterday.hour, minutes=yesterday.minute, seconds=yesterday.second,
-                                      microseconds=yesterday.microsecond)
+            now_utc = pendulum.now("utc")
+            return now_utc.subtract(days=1, hours=now_utc.hour, minutes=now_utc.minute, seconds=now_utc.second,
+                                    microseconds=now_utc.microsecond)
 
         @staticmethod
         def yesterday_usa_central_timezone():
             yesterday = pendulum.now("America/Chicago")
             return yesterday.subtract(days=1, hours=yesterday.hour, minutes=yesterday.minute, seconds=yesterday.second,
                                       microseconds=yesterday.microsecond)
+
 
     class RetryDelay:
         @staticmethod
