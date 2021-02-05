ff94bfd9d061265242d46cc3ecac40faa10ac856
Haegdorens, Gert <gert.haegdorens@yrcw.com>
2021-02-05 06:31:18 -0600

    Add retry delay

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -3,7 +3,7 @@ from zeppos_airflow.scheduler import Scheduler
 from datetime import datetime, timedelta
 
 class TestTheProjectMethods(unittest.TestCase):
-    def test_yesterday_methods(self):
+    def test_startdate_yesterday_method(self):
         now = datetime.now() - timedelta(days=1)
         self.assertEqual(now.year, Scheduler.StartDate.yesterday().year)
         self.assertEqual(now.month , Scheduler.StartDate.yesterday().month)
@@ -12,6 +12,9 @@ class TestTheProjectMethods(unittest.TestCase):
         self.assertEqual(0, Scheduler.StartDate.yesterday().minute)
         self.assertEqual(0, Scheduler.StartDate.yesterday().second)
         self.assertEqual(0, Scheduler.StartDate.yesterday().microsecond)
+
+    def test_retry_delay_five_minutes_method(self):
+        self.assertEqual('0:05:00', str(Scheduler.RetryDelay.five_minutes()))
 
 
 if __name__ == '__main__':
--- zeppos_airflow/scheduler.py
+++ zeppos_airflow/scheduler.py
@@ -1,5 +1,5 @@
 import pendulum
-
+from datetime import timedelta
 
 class Scheduler:
     class StartDate:
@@ -8,3 +8,8 @@ class Scheduler:
             yesterday = pendulum.now("America/Chicago")
             return yesterday.subtract(days=1, hours=yesterday.hour, minutes=yesterday.minute, seconds=yesterday.second,
                                       microseconds=yesterday.microsecond)
+
+    class RetryDelay:
+        @staticmethod
+        def five_minutes():
+            return timedelta(minutes=5)
